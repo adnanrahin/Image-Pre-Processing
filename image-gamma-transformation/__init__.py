8425a1ed7452ebdd57d1999d0f63c0a49f4e122f
Arx1971 <adnan.rahin13@gmail.com>
2019-11-25 03:25:43 -0500

    update

--- image-gamma-transformation/__init__.py
+++ image-gamma-transformation/__init__.py
@@ -1,3 +1,26 @@
-import cv2 as cv
 import numpy as np
+import matplotlib.pyplot as plt
+import cv2 as cv
+import PIL
+from spectral import *
+import math
+from laspy.file import File
+import matplotlib.pyplot as plt
+import matplotlib as mpl
 
+
+def gamma_transformation(path, gamma):
+    src = cv.imread(path)
+    gray = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
+    gamma_transposed_img = np.array(255 * (gray / 255) ** gamma, dtype='uint8')
+    img = PIL.Image.fromarray(gamma_transposed_img)
+    fileName = path[13: len(path)]
+    img.save('gamma_transposed' + fileName)
+
+
+overexposed = '../ImageData/overexpose.jpg'
+gamma_transformation(overexposed, 5)
+overexposed_img = cv.imread('gamma_transposedoverexpose.jpg')
+overexposed_histogram = cv.equalizeHist(overexposed_img)
+plt.plot(overexposed_histogram)
+plt.show()
