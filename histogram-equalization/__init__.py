211c4183985525edcaba87848dd56f5d639ccdc0
adnanrahin <adnan.rahin13@gmail.com>
2019-11-25 16:14:38 -0500

    Histogram Plotted

--- histogram-equalization/__init__.py
+++ histogram-equalization/__init__.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import cv2 as cv
+import cv2
 import PIL
 from spectral import *
 import math
@@ -9,13 +9,53 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 
 
-def histogram(path, filename):
-    src = cv.imread(path, 0)
-    img = cv.equalizeHist(src)
-    cv.imwrite(filename + '_img_hist.jpg', img)
+def calculate_histogram(source, bands):
+    width, height = source.shape[0:2]
+    histogram = np.zeros(256)
+    for i in range(height):
+        for j in range(width):
+            vector = source[j, i]
+            if bands == 3:
+                average = (int(vector[0]) + int(vector[1]) + int(vector[2])) / 3
+                histogram[int(average)] += 1
+            else:
+                histogram[vector[bands]] += 1
+
+    return np.array(histogram)
 
 
-overexposed = '../image-gamma-transformation/gamma_transposed_overexpose.jpg'
-histogram(overexposed, 'overexposed')
-underexposed = '../image-gamma-transformation/gamma_transposed_underexpose.jpg'
-histogram(underexposed, 'underexposed')
+def plot_histogram(img_matrix, plot_title):
+    color = ('r', 'g', 'b', 'k')
+    for i in range(0, len(color)):
+        hist = calculate_histogram(img_matrix, i)
+        plt.plot(hist, color=color[i])
+
+    plt.title(plot_title)
+    plt.savefig(plot_title + ".png")
+    plt.close()
+    img = PIL.Image.open(plot_title + '.png')
+    img.show()
+
+
+def histogram():
+    under_exposed = cv2.imread("underexposed_img_hist_eql.jpg")
+    plot_histogram(under_exposed, "underexposed_plot")
+    over_exposed = cv2.imread("overexposed_img_hist_eql.jpg")
+    plot_histogram(over_exposed, "overexposed_plot")
+
+
+def histogram_equalization(path, filename):
+    src = cv2.imread(path, 0)
+    img = cv2.equalizeHist(src)
+    cv2.imwrite(filename + '_img_hist_eql.jpg', img)
+
+
+def hist_equalizer():
+    overexposed = '../image-gamma-transformation/gamma_transposed_overexpose.jpg'
+    histogram_equalization(overexposed, 'overexposed')
+    underexposed = '../image-gamma-transformation/gamma_transposed_underexpose.jpg'
+    histogram_equalization(underexposed, 'underexposed')
+
+
+hist_equalizer()
+histogram()
--- image-gamma-transformation/__init__.py
+++ image-gamma-transformation/__init__.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import cv2 as cv
+import cv2
 import PIL
 from spectral import *
 import math
@@ -10,8 +10,8 @@ import matplotlib as mpl
 
 
 def gamma_transformation(path, gamma):
-    src = cv.imread(path)
-    gray = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
+    src = cv2.imread(path)
+    gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
     gamma_transposed_img = np.array(255 * (gray / 255) ** gamma, dtype='uint8')
     img = PIL.Image.fromarray(gamma_transposed_img)
     fileName = path[13: len(path)]
